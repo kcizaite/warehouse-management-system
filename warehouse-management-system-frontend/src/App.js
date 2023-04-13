f2b93885b8863ef106d882334c0f807dfb43c0b6
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-04-13 18:36:21 +0300

    other commits

---
+++ warehouse-management-system-frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {HashRouter, Route, Routes} from 'react-router-dom';
+import {ClientListPage} from "./pages/clients/ClientList";
+import {ClientCreatePage} from "./pages/clients/ClientCreate";
+import {ClientViewPage} from "./pages/clients/ClientView";
+import {InventoryCreatePage} from "./pages/inventories/InventoryCreate";
+
+function App() {
+    return (
+        <div>
+            <HashRouter>
+                <Routes>
+                    <Route path='/' element={<ClientListPage/>}/>
+                    {/* Client paths */}
+                    <Route path='/create-client' element={<ClientCreatePage/>}/>
+                    <Route path='/clients/view-client/:id' element={<ClientViewPage />} />
+                    {/* Inventory paths */}
+                    {/* Change path name */}
+                    <Route path='/clients/create-inventory' element={<InventoryCreatePage/>}/>
+                </Routes>
+            </HashRouter>
+        </div>
+    );
+}
+
+export default App;
