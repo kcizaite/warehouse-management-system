6e8778f44833ad5f7b5f9a10abbedea9d9b9d9be
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-04-13 18:33:49 +0300

    Created backend

---
+++ warehouse-management-system-backend/src/test/java/lt/techin/wms/warehousemanagementsystem/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.wms.warehousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
